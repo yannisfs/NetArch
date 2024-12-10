ca89191feb97527b0c140fb6bb9d81064ff1ed82
yannis.fs <yannis.fus@gmail.com>
2024-12-10 11:11:46 +0100

    models update and base hmtl added

--- company_site/contact_form/models.py
+++ company_site/contact_form/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    sent = models.DateField(auto_now_add=True)
+    company = models.CharField(max_length=64, default='')
+    name = models.CharField(max_length=128)
+    surname = models.CharField(max_length=128)
+    email  = models.EmailField()
+    content = models.CharField(max_length=4096)
--- company_site/previous_projects/models.py
+++ company_site/previous_projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=1024)
+    description = models.CharField(max_length=4096)
+    client = models.CharField(max_length=128, default='')
+    timeframe = models.Charfield(max_length=128, default='')
